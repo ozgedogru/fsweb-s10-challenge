d7b0f41fc314b8bf51fe1fe575a1dc8f76e56f6a
ozgedogru <ozgenurkarademir@gmail.com>
2023-11-25 16:09:57 +0300

    set up redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reducer } from "./reducers";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const reducers = combineReducers({ reducerNot });
 
+const store = createStore(reducer, applyMiddleware(thunk));
 
 root.render(
-  <BrowserRouter>
-    <>
-      <App />
-    </>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <>
+        <App />
+      </>
+    </BrowserRouter>
+  </Provider>
 );
