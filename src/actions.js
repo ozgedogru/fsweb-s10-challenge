330373214ebd5bb19ea6e3d67f5f888f14814ab0
ozgedogru <ozgenurkarademir@gmail.com>
2023-11-25 23:25:01 +0300

    delete button localstorage

--- src/actions.js
+++ src/actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const NOT_EKLE_API = "NOT_EKLE_API";
 export const NOT_SIL_API = "NOT_SIL_API";
+export const GET_INITIAL_NOTS = "GET_INITIAL_NOTS";
 
 export const notEkleAPI = (yeniNot) => (dispatch) => {
   axios
--- src/reducers.js
+++ src/reducers.js
@@ -1,29 +1,53 @@
-import { NOT_EKLE_API, NOT_SIL_API } from "./actions";
+import { NOT_EKLE_API, NOT_SIL_API, GET_INITIAL_NOTS } from "./actions";
 
 const s10chLocalStorageKey = "s10ch";
 
 const baslangicDegerleri = {
-  notlar: [
+  notlar: localStorageStateOku(s10chLocalStorageKey) || [
     {
       id: "75g1IyB8JLehAr0Lr5v3p",
       date: "Fri Feb 03 2023 09:40:27 GMT+0300 (GMT+03:00)",
       body: "Bugün hava çok güzel!|En iyi arkadaşımın en iyi arkadaşı olduğumu öğrendim :)|Kedim iyileşti!",
     },
   ],
+  // notlar: [
+  //   {
+  //     id: "75g1IyB8JLehAr0Lr5v3p",
+  //     date: "Fri Feb 03 2023 09:40:27 GMT+0300 (GMT+03:00)",
+  //     body: "Bugün hava çok güzel!|En iyi arkadaşımın en iyi arkadaşı olduğumu öğrendim :)|Kedim iyileşti!",
+  //   },
+  // ],
 };
 
 export const reducer = (state = baslangicDegerleri, action) => {
   switch (action.type) {
     case NOT_EKLE_API:
+      localStorageStateYaz(s10chLocalStorageKey, [
+        ...state.notlar,
+        action.payload,
+      ]);
       return {
         ...state,
         notlar: [...state.notlar, action.payload],
       };
 
     case NOT_SIL_API:
+      const updateNotes = localStorageStateOku(s10chLocalStorageKey).filter(
+        (item) => item.id !== action.payload
+      );
+      localStorageStateYaz(s10chLocalStorageKey, updateNotes);
+
       return {
         ...state,
         notlar: state.notlar.filter((item) => item.id !== action.payload),
+      };
+
+    case GET_INITIAL_NOTS:
+      const initial = baslangicNotlariniGetir(s10chLocalStorageKey);
+      console.log(initial);
+      return {
+        ...state,
+        notlar: initial ? initial.notlar : state.notlar,
       };
 
     default:
@@ -39,7 +63,7 @@ function localStorageStateOku(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-function baslangicNotlariniGetir(key) {
+export function baslangicNotlariniGetir(key) {
   const eskiNotlar = localStorage.getItem(key);
 
   if (eskiNotlar) {
