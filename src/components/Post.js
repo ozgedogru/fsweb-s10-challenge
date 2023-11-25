0627b5d0c32c3dce73a624554dd429c485d647ef
ozgedogru <ozgenurkarademir@gmail.com>
2023-11-25 16:15:48 +0300

    activates delete button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { formatDistanceToNow } from "date-fns";
 import { tr } from "date-fns/locale";
+import { useDispatch } from "react-redux";
+import { notSil } from "../actions";
 
 export default function Post({ item }) {
+  const dispatch = useDispatch();
 
-  function handleSil() {
-    // burada ilgili eylemi dispatch edin
+  function handleSil(notId) {
+    dispatch(notSil(notId));
     // sonra toast mesajı gösterin
   }
 
@@ -24,7 +27,10 @@ export default function Post({ item }) {
         </p>
       ))}
 
-      <button className="text-xs text-amber-600 mt-4 underline" onClick={handleSil}>
+      <button
+        className="text-xs text-amber-600 mt-4 underline"
+        onClick={handleSil}
+      >
         Bu notu sil
       </button>
     </div>
